14cd382822b9adf13fe70429eb7a2264b903bede
Ranjth chalicheemala <b5t4fnzrj7@privaterelay.appleid.com>
2026-06-08 21:04:12 -0400

    script.js

--- script-3.js
+++ script-3.js
@@ -1,6 +1,6 @@
 const target=new Date('July 5, 2026 09:00:00').getTime();
 setInterval(()=>{
-const now=new Date().getTime();
+const now=new Date().getTime(); 
 const d=target-now;
 const days=Math.floor(d/(1000*60*60*24));
 document.getElementById('countdown').innerHTML=days+' days to go';
